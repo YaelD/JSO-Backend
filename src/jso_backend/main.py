b8187f6540486f67b992636bbb18f0b71cf0a407
Yael Davidov <yael.davidov97@gmail.com>
2023-09-28 22:14:07 +0300

    Create DatabaseManager class and delete previous models
    - Create singleton DatabaseManager to handle creating the database connection, tables and SQLModel Sessions
    - Delete the database.py module
    - Delete redundent models.
    - Change main.py to use the DatabaseManager

--- src/jso_backend/main.py
+++ src/jso_backend/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 
-from jso_backend.database import create_db_and_tables
+from jso_backend.data_access.database_manager import DatabaseManager
+
+from .routers import jobs_router
 
 app = FastAPI()
 
 
+app.include_router(jobs_router.router)
+
+
 @app.on_event("startup")
 def on_startup():
-    create_db_and_tables()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
+    db_manager = DatabaseManager()
+    db_manager.create_db_and_tables()
