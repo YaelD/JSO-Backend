9778c69ecb23e84ebed7204d4c4264cc684d7a8a
Yael Davidov <yael.davidov97@gmail.com>
2023-10-11 19:35:49 +0300

    Update Business Logic package
    - Create services package.
    - Create validators package.
    - Create job_service module for handling transactions.
    - Create job_validator module for handling logic validations of the job object.

---
+++ src/jso_backend/business_logic/services/job_service.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+from jso_backend.business_logic.validators.job_validator import JobValidator
+from jso_backend.data_access.job_data_access import JobDataAccess
+from jso_backend.data_access.unit_of_work import UnitOfWork
+from jso_backend.domain.job_entity import JobEntity
+
+
+class JobService:
+    async def get_job_by_id(self, id: int) -> JobEntity:
+        async with UnitOfWork().create() as unit_of_work:
+            job: JobEntity = await JobDataAccess(session=unit_of_work).get_job_by_id(job_id=id)
+            return job
+
+    async def get_all_jobs(self) -> list[JobEntity]:
+        async with UnitOfWork().create() as unit_of_work:
+            all_jobs: list[JobEntity] = await JobDataAccess(unit_of_work).get_list()
+            return all_jobs
+
+    async def create_job(self, job: JobEntity) -> JobEntity:
+        async with UnitOfWork().create() as unit_of_work:
+            created_job: JobEntity = await JobDataAccess(unit_of_work).create_job(job)
+            return created_job
+
+    async def update_job(self, id: int, job_details: dict[str, Any]) -> JobEntity:
+        async with UnitOfWork().create() as unit_of_work:
+            job_access_data: JobDataAccess = JobDataAccess(unit_of_work)
+            job_to_update: JobEntity = await job_access_data.get_job_by_id(job_id=id)
+            JobValidator().validate_and_update_job_data_fields(
+                job_entity=job_to_update, job_details_to_update=job_details
+            )
+            updated_job: JobEntity = await job_access_data.update_job(
+                job_id=id, job_data=job_details
+            )
+            return updated_job
+
+    async def delete_job_by_id(self, id: int) -> None:
+        async with UnitOfWork().create() as unit_of_work:
+            job_data_access: JobDataAccess = JobDataAccess(session=unit_of_work)
+            await job_data_access.get_job_by_id(job_id=id)
+            job_data_access.delete_job(job_id=id)
